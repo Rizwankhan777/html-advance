cb85e273fb68f241692c4f5294f9a12af4949792
Rizwan khan <47868436+Rizwankhan777@users.noreply.github.com>
2022-06-22 16:48:03 +0500

    Add files via upload

---
+++ Hotel-design/food-app/assets/js/custom.js
@@ -0,0 +1,80 @@
+// Animations call 
+
+// AOS.init({
+//     duration: 1200,
+//     once: true,
+// })
+
+// new WOW().init();
+
+
+// end Animation 
+
+
+
+
+
+
+// counter start at six-wrap class 
+// JS 
+
+// $(window).on('scroll', function () {
+//     if ($(window).scrollTop() >= $('.six-wrap').offset().top + $('.six-wrap').outerHeight() - window.innerHeight){
+//         counterrr();
+//     }
+// });
+
+
+// function counterrr() {
+//     $('.counter').each(function () {
+//         $(this).prop('Counter', 0).animate({
+//             Counter: $(this).text()
+//         }, {
+//             duration: 3000,
+//             easing: 'swing',
+//             step: function (now) {
+//                 $(this).text(Math.ceil(now));
+//             }
+//         });
+//     });
+//     $('.counter1').removeClass('counter');
+// }
+
+// close 
+
+$("[data-fancybox]").fancybox({
+    AnimationEffect : "slide"
+})
+
+
+
+
+
+
+
+
+
+
+
+// scroll behavior 
+// window.scroll({
+//     behavior: 'smooth'
+// });
+
+
+
+
+// collapse hide on complete body click expert header-menulink-main 
+
+// $(document).on('click', function (event) {
+//     if (!$(event.target).closest('.header-menulinks-main').length) {
+//         $('.collapse').collapse('hide');
+//     }
+// });
+
+
+// inner click closing in  industry-inner class 
+
+// $(".industry-inner").click(function () {
+//     $('.collapse').collapse('hide');
+// });
