07ddfa57e8999c67d1e6fbe1a91f03cd79048bdb
Rizwan khan <47868436+Rizwankhan777@users.noreply.github.com>
2022-06-23 20:25:53 +0500

    Add files via upload

---
+++ fruit-app/js/custom.js
@@ -0,0 +1,19 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    nav:false,
+    dots:false,
+    margin:10,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+
