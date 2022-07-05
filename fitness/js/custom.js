3581d81ca3a09ff5b0f5eccf2411c5be359d4637
Rizwan khan <47868436+Rizwankhan777@users.noreply.github.com>
2022-07-05 21:23:12 +0500

    Add files via upload

---
+++ fitness/js/custom.js
@@ -0,0 +1,50 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    dots:false,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+var selector = $('.owl-carousel');
+$('.my-next-button').click(function() {
+  selector.trigger('next.owl.carousel');
+});
+
+$('.my-prev-button').click(function() {
+  selector.trigger('prev.owl.carousel');
+});
+
+
+
+
+
+$('.testimonial-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+
+
